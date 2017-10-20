9ba1424d5e9a40838e755f18c8873ddd5c89cd82
Margarita R <margarita@arrayschool.com>
2017-10-20 14:29:31 -0600

    Added <hr>'s as separators between comments.  Made button colors consistent.  Made text sizing and line ht consistent.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,6 @@
 class ArticlesController < ApplicationController
 
-  http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
+  http_basic_authenticate_with name: "margarita", password: "1223", except: [:index, :show]
 
 def index
   @articles = Article.all
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,6 @@
 class CommentsController < ApplicationController
 
-  http_basic_authenticate_with name: "dhh", password: "secret", only: :destroy
+  http_basic_authenticate_with name: "margarita", password: "1223", only: :destroy
 
   def create
     @article = Article.find(params[:article_id])
